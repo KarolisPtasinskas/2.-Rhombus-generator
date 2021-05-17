e5287f17bce2bb49fd9e41c261f36ec5b0ff51ea
Karolis <k.ptasinskas@gmail.com>
2021-05-17 16:57:09 +0300

    Revert2

--- functions.js
+++ functions.js
@@ -1,12 +1,8 @@
 /////////////////////////////////////////////////////////////////
 ////
-//// >>>>>>>>>>> HTML version, with input, colors and new figures <<<<<<<<<<<<
+//// >>>>>>>>>>> HTML version, with input <<<<<<<<<<<<
 ////
 /////////////////////////////////////////////////////////////////
-
-function whichFigure() {
-    let figure = document.querySelector('').value;
-    return figure;
 
 document.querySelector('#btn').addEventListener('click', function () {
     let number = Number(document.querySelector('#fname').value);
@@ -21,24 +17,14 @@ function makeRhombus(number) {
     for (let i = 1; i < number + 1; i++) {
         if(i <= Math.floor(number / 2)) {
             let repeatSpace = Math.round(number / 2) - i;
-<<<<<<< HEAD
-            let row = space.repeat(repeatSpace) + symbol + space.repeat(repeatSpace);
-            let rowHTML = `<p${colorManagement()}>${row}</p$>`
-=======
             let row = space.repeat(repeatSpace) + symbol;
-            let rowHTML = `<p>${row}</p>`
->>>>>>> parent of 37028a0 (Added color function)
+            let rowHTML = `<p${colorManagement()}>${row}</p$>`
             document.getElementById('konteineris').innerHTML += rowHTML;
             symbol += '**';
         } else {
             let repeatSpace = i - Math.round(number / 2);
-<<<<<<< HEAD
-            let row = space.repeat(repeatSpace) + symbol + space.repeat(repeatSpace);
-            let rowHTML = `<p${colorManagement()}>${row}</p$>`
-=======
             let row = space.repeat(repeatSpace) + symbol;
-            let rowHTML = `<p>${row}</p>`
->>>>>>> parent of 37028a0 (Added color function)
+            let rowHTML = `<p${colorManagement()}>${row}</p$>`
             document.getElementById('konteineris').innerHTML += rowHTML;
             symbol = symbol.substring(0, symbol.length - 2);
         }
@@ -46,7 +32,6 @@ function makeRhombus(number) {
     }
 }
 
-<<<<<<< HEAD
 function colorManagement() {
     let colorCheck = document.querySelector('#color-check').checked;
     let colorTag = '';
@@ -58,51 +43,7 @@ function colorManagement() {
 }
 
 
-/////////////////////////////////////////////////////////////////
-////
-//// >>>>>>>>>>> HTML version, with input and colors <<<<<<<<<<<<
-////
-/////////////////////////////////////////////////////////////////
 
-// document.querySelector('#btn').addEventListener('click', function () {
-//     let number = Number(document.querySelector('#fname').value);
-//     // console.log(number, typeof number);
-//     number % 2 === 1 ? makeRhombus(number) : document.getElementById('konteineris').innerHTML = `<h1>Skaičius netinkamas</h1>`;
-//     });
-
-// function makeRhombus(number) {
-//     document.getElementById('konteineris').innerHTML = ``;
-//     let symbol = '*';
-//     let space = '&nbsp;&nbsp;';
-//     for (let i = 1; i < number + 1; i++) {
-//         if(i <= Math.floor(number / 2)) {
-//             let repeatSpace = Math.round(number / 2) - i;
-//             let row = space.repeat(repeatSpace) + symbol + space.repeat(repeatSpace);
-//             let rowHTML = `<p${colorManagement()}>${row}</p$>`
-//             document.getElementById('konteineris').innerHTML += rowHTML;
-//             symbol += '**';
-//         } else {
-//             let repeatSpace = i - Math.round(number / 2);
-//             let row = space.repeat(repeatSpace) + symbol + space.repeat(repeatSpace);
-//             let rowHTML = `<p${colorManagement()}>${row}</p$>`
-//             document.getElementById('konteineris').innerHTML += rowHTML;
-//             symbol = symbol.substring(0, symbol.length - 2);
-//         }
-        
-//     }
-// }
-
-// function colorManagement() {
-//     let colorCheck = document.querySelector('#color-check').checked;
-//     let colorTag = '';
-//     function randomColor() {
-//         let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-//         return randomColor;
-//     }
-//     return colorCheck ? colorTag = ` style="color: ${randomColor()};"` : colorTag = ``;
-// }
-=======
->>>>>>> parent of 37028a0 (Added color function)
 
 /////////////////////////////////////////////////////////////////
 ////
